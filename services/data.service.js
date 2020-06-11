9775ab22261b64928c91faf1d3123d1e1e54fc42
TyrantInShadows <lazarp@elfak.rs>
2020-06-11 17:14:46 +0200

    small changes

--- services/data.service.js
+++ services/data.service.js
@@ -8,7 +8,7 @@ module.exports = {
   actions: {
     readCO: {
       params: {
-        sensorId: { type: "number" }
+        sensorId: { type: "number" },
       },
       async handler(ctx) {
         try {
@@ -20,11 +20,11 @@ module.exports = {
           console.log(err);
           return null;
         }
-      }
+      },
     },
     readNO2: {
       params: {
-        sensorId: { type: "number" }
+        sensorId: { type: "number" },
       },
       async handler(ctx) {
         try {
@@ -36,11 +36,11 @@ module.exports = {
           console.log(err);
           return null;
         }
-      }
+      },
     },
     readSO2: {
       params: {
-        sensorId: { type: "number" }
+        sensorId: { type: "number" },
       },
       async handler(ctx) {
         try {
@@ -52,8 +52,8 @@ module.exports = {
           console.log(err);
           return null;
         }
-      }
-    }
+      },
+    },
   },
   methods: {
     sendParameters(
@@ -72,13 +72,13 @@ module.exports = {
         SO2_AQI: SO2_AQI,
         NO2_Con: NO2_Con,
         NO2_AQI: NO2_AQI,
-        sensorId: sensorId
+        sensorId: sensorId,
       };
       console.log(body);
       request.post(
         process.env.ANALYTICS_URL,
         {
-          json: body
+          json: body,
         },
         (err, res, body) => {
           if (err) {
@@ -89,7 +89,7 @@ module.exports = {
           console.log(body);
         }
       );
-    }
+    },
   },
   events: {
     "device.parametersRead": {
@@ -106,9 +106,9 @@ module.exports = {
               sensorId: payload.sensorId,
               CO_Con: payload.CO_Con,
               CO_AQI: payload.CO_AQI,
-              CO_AQS_Name: payload.CO_AQS_Name
+              CO_AQS_Name: payload.CO_AQS_Name,
             },
-            time: payload.Time
+            time: payload.Time,
           },
           {
             measurement: "so2",
@@ -116,9 +116,9 @@ module.exports = {
               sensorId: payload.sensorId,
               SO2_Con: payload.SO2_Con,
               SO2_AQI: payload.SO2_AQI,
-              SO2_AQS_Name: payload.SO2_AQS_Name
+              SO2_AQS_Name: payload.SO2_AQS_Name,
             },
-            time: payload.Time
+            time: payload.Time,
           },
           {
             measurement: "no2",
@@ -126,10 +126,10 @@ module.exports = {
               sensorId: payload.sensorId,
               NO2_Con: payload.NO2_Con,
               NO2_AQI: payload.NO2_AQI,
-              NO2_AQS_Name: payload.NO2_AQS_Name
+              NO2_AQS_Name: payload.NO2_AQS_Name,
             },
-            time: payload.Time
-          }
+            time: payload.Time,
+          },
         ]);
         this.sendParameters(
           payload.CO_Con,
@@ -140,8 +140,8 @@ module.exports = {
           payload.NO2_AQI,
           payload.sensorId
         );
-      }
-    }
+      },
+    },
   },
   created() {
     this.influx = new Influx.InfluxDB({
@@ -156,9 +156,9 @@ module.exports = {
             sensorId: Influx.FieldType.INTEGER,
             CO_Con: Influx.FieldType.FLOAT,
             CO_AQI: Influx.FieldType.FLOAT,
-            CO_AQS_Name: Influx.FieldType.STRING
+            CO_AQS_Name: Influx.FieldType.STRING,
           },
-          tags: ["host"]
+          tags: ["host"],
         },
         {
           measurement: "so2",
@@ -166,9 +166,9 @@ module.exports = {
             sensorId: Influx.FieldType.INTEGER,
             SO2_Con: Influx.FieldType.FLOAT,
             SO2_AQI: Influx.FieldType.FLOAT,
-            SO2_AQS_Name: Influx.FieldType.STRING
+            SO2_AQS_Name: Influx.FieldType.STRING,
           },
-          tags: ["host"]
+          tags: ["host"],
         },
         {
           measurement: "no2",
@@ -176,17 +176,17 @@ module.exports = {
             sensorId: Influx.FieldType.INTEGER,
             NO2_Con: Influx.FieldType.FLOAT,
             NO2_AQI: Influx.FieldType.FLOAT,
-            NO2_AQS_Name: Influx.FieldType.STRING
+            NO2_AQS_Name: Influx.FieldType.STRING,
           },
-          tags: ["host"]
-        }
-      ]
+          tags: ["host"],
+        },
+      ],
     });
-    this.influx.getDatabaseNames().then(names => {
+    this.influx.getDatabaseNames().then((names) => {
       if (!names.includes("air_emission")) {
         return this.influx.createDatabase("air_emission");
       }
       return null;
     });
-  }
+  },
 };
--- services/device.service.js
+++ services/device.service.js
@@ -27,7 +27,7 @@ module.exports = {
           SO2_Con: recordSO2.SO2_Con,
           SO2_AQI: recordSO2.AQI_VALUE,
           SO2_AQS_Name: recordSO2.AQS_PARAMETER_DESC,
-          Time: Date.now()
+          Time: Date.now(),
         });
       }, this.interval);
     },
@@ -47,7 +47,7 @@ module.exports = {
         this.dataSO2 = JSON.parse(data);
         console.log(this.dataSO2.length);
       });
-    }
+    },
   },
   events: {
     "device.turnCOCleanerOn": {
@@ -55,21 +55,21 @@ module.exports = {
       handler(payload) {
         console.log('Received "turnCOCleanerOn" event: CO cleaner turned ON');
         this.CleanerCO = true;
-      }
+      },
     },
     "device.turnCOCleanerOff": {
       group: "other",
       handler(payload) {
         console.log('Received "turnCOCleanerOff" event: CO cleaner turned OFF');
         this.CleanerCO = false;
-      }
+      },
     },
     "device.turnSO2CleanerOn": {
       group: "other",
       handler(payload) {
         console.log('Received "turnSO2CleanerOn" event: SO2 cleaner turned ON');
         this.CleanerSO2 = true;
-      }
+      },
     },
     "device.turnSO2CleanerOff": {
       group: "other",
@@ -78,14 +78,14 @@ module.exports = {
           'Received "turnSO2CleanerOff" event: SO2 cleaner turned OFF'
         );
         this.CleanerSO2 = false;
-      }
+      },
     },
     "device.turnNO2CleanerOn": {
       group: "other",
       handler(payload) {
         console.log('Received "turnNO2CleanerOn" event: NO2 cleaner turned ON');
         this.CleanerNO2 = true;
-      }
+      },
     },
     "device.turnNO2CleanerOff": {
       group: "other",
@@ -94,7 +94,7 @@ module.exports = {
           'Received "turnNO2CleanerOff" event: NO2 cleaner turned OFF'
         );
         this.CleanerNO2 = false;
-      }
+      },
     },
     "device.changeCOCleanerLvl": {
       group: "other",
@@ -113,7 +113,7 @@ module.exports = {
             `CO cleaner must be between 0 and 100%, provided: ${payload.Lvl}`
           );
         }
-      }
+      },
     },
     "device.changeSO2CleanerLvl": {
       group: "other",
@@ -130,7 +130,7 @@ module.exports = {
             `CO cleaner must be between 0 and 100%, provided: ${payload.Lvl}`
           );
         }
-      }
+      },
     },
     "device.changeNO2CleanerLvl": {
       group: "other",
@@ -147,8 +147,8 @@ module.exports = {
             `CO cleaner must be between 0 and 100%, provided: ${payload.Lvl}`
           );
         }
-      }
-    }
+      },
+    },
   },
   created() {
     // actuator
@@ -171,5 +171,5 @@ module.exports = {
     // init
     this.readData();
     this.init();
-  }
+  },
 };
