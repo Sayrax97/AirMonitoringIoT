52b8ca47cef653705475b07ceee5a1b832c0f922
TyrantInShadows <lazarp@elfak.rs>
2020-06-27 19:58:23 +0200

    dashbord bug fix

--- dashboard/src/app/app.component.ts
+++ dashboard/src/app/app.component.ts
@@ -29,9 +29,13 @@ export class AppComponent implements OnInit {
   dataPointsCOAQI = [];
   dataPointsSO2AQI = [];
   dataPointsNO2AQI = [];
-  dpsLength: number;
+  dpsLengthCO: number;
+  dpsLengthSO2: number;
+  dpsLengthNO2: number;
   constructor(private gServices: GatewayService) {
-    this.dpsLength = 0;
+    this.dpsLengthCO = 0;
+    this.dpsLengthNO2=0;
+    this.dpsLengthSO2=0;
   }
 
   ngOnInit(): void {
@@ -84,77 +88,103 @@ export class AppComponent implements OnInit {
         },
       ],
     });
-
-    this.gServices.getStats().subscribe((res) => {
-      this.stats = res;
-      this.updateChart(this.stats.sendorID, chartCO, chartSO2, chartNO2);
-    });
+    this.updateChart(chartCO, chartSO2, chartNO2);
     this.gServices.getMaxValue().subscribe((res) => {
       this.maxValue = res[0];
     });
   }
 
   updateChart(
-    id: number,
     chart1: CanvasJS.Chart,
     chart2: CanvasJS.Chart,
     chart3: CanvasJS.Chart
   ) {
     let that = this;
-    this.gServices.getSO2Sp(this.stats.sendorID).subscribe((res) => {
-      this.so2Sp = res[0];
-      this.dataPointsSO2.push({
-        x: this.dataPointsSO2.length,
-        y: this.so2Sp.SO2_Con,
-      });
-      this.dataPointsSO2AQI.push({
-        x: this.dataPointsSO2AQI.length,
-        y: this.so2Sp.SO2_AQI,
-      });
-      //console.log(this.so2Sp.SO2_Con);
-      if (this.dataPointsSO2.length > 20) {
-        this.dataPointsSO2.shift();
-        this.dataPointsSO2AQI.shift();
-      }
-      chart1.render();
-    });
-    this.gServices.getCOSp(this.stats.sendorID).subscribe((res) => {
-      this.coSp = res[0];
-      this.dataPointsCO.push({
-        x: this.dataPointsCO.length,
-        y: this.coSp.CO_Con,
-      });
-      this.dataPointsCOAQI.push({
-        x: this.dataPointsCOAQI.length,
-        y: this.coSp.CO_AQI,
-      });
+    this.gServices.getStats().subscribe((res) => {
+      this.stats = res;
 
-      if (this.dataPointsCO.length > 20) {
-        this.dataPointsCO.shift();
-        this.dataPointsCOAQI.shift();
-      }
-      chart2.render();
-    });
-    this.gServices.getNO2Sp(this.stats.sendorID).subscribe((res) => {
-      this.no2Sp = res[0];
-      this.dataPointsNO2.push({
-        x: this.dataPointsNO2.length,
-        y: this.no2Sp.NO2_Con,
-      });
-      this.dataPointsNO2AQI.push({
-        x: this.dataPointsNO2AQI.length,
-        y: this.no2Sp.NO2_AQI,
-      });
-      //console.log(this.no2Sp.NO2_Con);
-      if (this.dataPointsNO2.length > 20) {
-        this.dataPointsNO2.shift();
-        this.dataPointsNO2AQI.shift();
-      }
-      chart3.render();
-    });
+      document.getElementById(
+        "senzorID"
+      ).innerHTML = this.stats.sendorID.toString();
 
+      if (this.stats.CleanerCO)
+        document.getElementById("CleanerCO").innerHTML = "On";
+      else document.getElementById("CleanerCO").innerHTML = "Off";
+      document.getElementById(
+        "CleanerCOLvl"
+      ).innerHTML = this.stats.CleanerCOLvl.toString();
+
+      if (this.stats.CleanerNO2)
+        document.getElementById("CleanerNO2").innerHTML = "On";
+      else document.getElementById("CleanerNO2").innerHTML = "Off";
+      document.getElementById(
+        "CleanerNO2Lvl"
+      ).innerHTML = this.stats.CleanerNO2Lvl.toString();
+
+      if (this.stats.CleanerSO2)
+        document.getElementById("CleanerSO2").innerHTML = "On";
+      else document.getElementById("CleanerSO2").innerHTML = "Off";
+      document.getElementById(
+        "CleanerSO2Lvl"
+      ).innerHTML = this.stats.CleanerSO2Lvl.toString();
+
+      this.gServices.getSO2Sp(this.stats.sendorID).subscribe((res) => {
+        this.so2Sp = res[0];
+        this.dataPointsSO2.push({
+          x: this.dpsLengthSO2,
+          y: this.so2Sp.SO2_Con,
+        });
+        this.dataPointsSO2AQI.push({
+          x: this.dpsLengthSO2,
+          y: this.so2Sp.SO2_AQI,
+        });
+        //console.log(this.so2Sp.SO2_Con);
+        if (this.dataPointsSO2.length > 20) {
+          this.dataPointsSO2.shift();
+          this.dataPointsSO2AQI.shift();
+        }
+        chart1.render();
+      });
+      this.gServices.getCOSp(this.stats.sendorID).subscribe((res) => {
+        this.coSp = res[0];
+        this.dataPointsCO.push({
+          x: this.dpsLengthCO,
+          y: this.coSp.CO_Con,
+        });
+        this.dataPointsCOAQI.push({
+          x: this.dpsLengthCO,
+          y: this.coSp.CO_AQI,
+        });
+
+        if (this.dataPointsCO.length > 20) {
+          this.dataPointsCO.shift();
+          this.dataPointsCOAQI.shift();
+        }
+        chart2.render();
+      });
+      this.gServices.getNO2Sp(this.stats.sendorID).subscribe((res) => {
+        this.no2Sp = res[0];
+        this.dataPointsNO2.push({
+          x: this.dpsLengthNO2,
+          y: this.no2Sp.NO2_Con,
+        });
+        this.dataPointsNO2AQI.push({
+          x: this.dpsLengthNO2,
+          y: this.no2Sp.NO2_AQI,
+        });
+        //console.log(this.no2Sp.NO2_Con);
+        if (this.dataPointsNO2.length > 20) {
+          this.dataPointsNO2.shift();
+          this.dataPointsNO2AQI.shift();
+        }
+        chart3.render();
+      });
+    });
+    this.dpsLengthCO++;
+    this.dpsLengthNO2++;
+    this.dpsLengthSO2++;
     setTimeout(function () {
-      that.updateChart(id, chart1, chart2, chart3);
+      that.updateChart(chart1, chart2, chart3);
     }, 5000);
   }
 }
